68b6ebef26ae153b6c3ba406b8d80b9caf41c3f6
Yewert <blazer.andrew@gmail.com>
2020-03-15 18:35:14 +0500

    added main activity and recycler view

---
+++ app/src/main/java/com/example/hw03/HabbitsList.kt
@@ -0,0 +1,12 @@
+package com.example.hw03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HabbitsList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_habbits_list)
+    }
+}
